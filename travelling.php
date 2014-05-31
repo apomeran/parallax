d5b47563ceaa24fc44599f2610b14e0bae54768b
unknown <diecarrion@gmail.com>
2014-05-31 20:56:46 -0300

    travel page beginning

---
+++ travelling.php
@@ -0,0 +1,130 @@
+<?php include_once("includes/head.php") ?>
+<div>
+    <?php include_once("includes/header.php") ?>
+	<img style="width: 100% !important; "src="img/travel/top-img.png"></img>
+</div>
+
+<section id="separator-section">
+		<div class="sqs-layout">
+			  <div class="row">
+				<div class="col sqs-col-14">
+					  <div class="row">
+									<div class="col sqs-col-14-margin">
+									<div class="sqs-block html-block sqs-block-html">
+										<div class="sqs-block-content">									
+											<hr></hr>
+											<span class="text-primary">
+											IT IS REALLY VERY EASY TO GET TO PARADISE LODGE!</span>
+											<hr></hr>
+										</div>
+									</div>
+									</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="sqs-layout">
+					<div class="row">
+						  <div class="col sqs-col-16 lodge-sect">						
+								<div class="row sqs-row">
+								<div class="col sqs-col-8">
+									<div class="sqs-block html-block sqs-block-html">
+										<div class="sqs-block-content">
+										<span>Most major airlines have flights into Cancun from just about every major city in the world. Los Angeles, Dallas, Houston, Atlanta and Miami, all, have non-stop flights to Cancun. Unless you plan to spend a few nights in Cancun before you go to Paradise Lodge, you need to arrive in Cancun before 2:00PM for a transfer on the same day. If you do arrive a day or two earlier and want to be picked up at your hotel, it is no problem. We'll just need to know where you will be staying. You'll need a passport. An expiration date of at least 6 months beyond your travel dates is always a</span>
+										</div>
+									</div>
+								</div>
+								<div class="col sqs-col-8">
+									<div class="sqs-block html-block sqs-block-html">
+										<div class="sqs-block-content">
+											<span>least 6 months beyond your travel dates is always a good idea when traveling abroad. As with any international travel, check with your family doctor for health updates for the region you are visiting for any vaccines or special medicines you might want or need. When you arrive at the airport in Cancun a Paradise Lodge and after proceeding through customs, a lodge representative will meet and drive you and any other guests arriving at the same day in an air-conditioned van through the southern Mexican Yucatan directly to the lodge to arrive in time for dinner.</span>
+										</div>
+									</div>
+								</div>
+								</div>
+							</div>
+				</div>
+			</div>
+	</section>
+	<!-- aca viene la section map 1-->
+	<!--<section id="bg-section" class="parallax" data-image="img/travel/map-1.jpg" data-width="2560" data-height="1567">
+		<div class="centered-caption-label">
+			<img src="img/travel/map-1.jpg"></img>
+		<div>	
+	</section>-->
+	
+	
+						<!--<section id="separator-section">
+						<div class="row">
+							  <div class="col sqs-col-16 lodge-sect">
+									<div class="row sqs-row">
+									<div class="col sqs-col-8">
+										<div class="sqs-block html-block sqs-block-html">
+											<div class="sqs-block-content">
+												<span>It is an interesting drive starting at first in the touristy region of Cancun, then traveling south through the heart of the Mayan country and further south to more rural and farm areas and finally, arriving at the coast. It is a paved highway and road for almost the entire way. The last few miles are on a gravel/dirt coastal road that takes you directly to the lodge. The lodge itself is waiting on a beautiful piece of property overlooking the ocean and the reef. After arrival, your luggage will be delivered to your</span>
+											</div>
+										</div>
+									</div>
+									<div class="col sqs-col-8">
+										<div class="sqs-block html-block sqs-block-html">
+											<div class="sqs-block-content">
+												<span>After arrival, your luggage will be delivered to your room; introductions to the manager and staff will be made, you'll be served a cool drink and a delicious dinner. The manager will let you know the fishing and lodge schedules and answer questions about your upcoming week. At the end of your stay, you will need an airline departure from Cancun after 1:00 p.m.You will leave the lodge very early to make sure you are in time for your flight.</span>
+											</div>
+										</div>
+									</div>
+									</div>
+								</div>
+						</div>
+						</section>-->			
+						
+						<!-- aca viene la section map 2-->
+						<!--<section id="bg-section" class="parallax" data-image="img/travel/map-2.jpg" data-width="2560" data-height="1600">
+							<div class="centered-caption-label">
+								<img src="img/travel/map-2.png"></img>
+							<div>	
+						</section>-->
+						
+						<!--<section id="separator-section">
+						<div class="row">
+							  <div class="col sqs-col-16 lodge-sect">
+									<div class="row sqs-row">
+									<div class="col sqs-col-8">
+										<div class="sqs-block html-block sqs-block-html">
+											<div class="sqs-block-content">
+												<span>You will receive, after your trip confirmation, a complete and thorough pre-trip and travel planner well before your departure date to help you with the details. And, of course, you are encouraged to call our</span>
+											</div>
+										</div>
+									</div>
+									<div class="col sqs-col-8">
+										<div class="sqs-block html-block sqs-block-html">
+											<div class="sqs-block-content">
+												<span>travel department with any questions you may have about Paradise Lodge. All of our travel specialists have stayed and fished there and are familiar with international travel packing fishing gear.</span>
+											</div>
+										</div>
+									</div>
+									</div>
+								</div>
+						</div>
+						</section>-->
+						
+				
+				<!--	</div>
+				</div>
+		 </div>-->
+		 
+	
+
+
+<script src="js/vendors/jquery.min.js"></script>
+<script src="js/hw-parallax.min.js"></script>
+<script>
+  $('.parallax').parallax({
+    scroll_factor: 0.5
+  });
+</script>
+
+<!--<footer id="footer">
+<div class="back-to-top-link"><a href="#top"><span class="arrow"></span></a></div>
+<?php include_once("includes/footer-no-homepage.php") ?>
+</footer>-->
+</body></html>
