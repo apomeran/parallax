6f90977c51371896715c188074488083af0cf16a
unknown <alan_pomerantz@hotmail.com>
2014-05-31 16:31:26 -0300

    Fixed hr in footernohomepage

--- includes/footer-no-homepage.php
+++ includes/footer-no-homepage.php
@@ -1,40 +1,46 @@
-	<div class="about-us-container">
-		<div class="sqs-layout">
-				<div class="row sqs-row normalize">
-				  <div class="col sqs-col-14">
-						<div class="row row-footer">
-						<div class="col sqs-col-6">
-							<div class="sqs-block html-block sqs-block-html">
-								<div class="sqs-block-content">
-								<div class="footer-logos"><img src="img/twitter2.png"></img><img src="img/fblogo2.png"></img></div>
-								<div class="about-us-title">PARADISE LODGE IS BOOKED EXCLUSIVELY<br/> THROUGH THE FLY SHOP&reg;</div>
-								</div>
-							</div>
-						</div>
-						<div class="col sqs-col-4">
-							<div class="sqs-block html-block sqs-block-html">
-								<div class="sqs-block-content">
-								<hr></hr>
-									<div class="about-us-subtitle">The Fly Shop, Inc.</div>
-									4140 Churn Creek Road<br/>
-									Redding, CA 96002<br/>
-									800-699-3474 www.theflyshop.com
-								</div>
-							</div>
-						</div>
-						<div class="col sqs-col-4">
-							<div class="sqs-block html-block sqs-block-html">
-								<div class="sqs-block-content">
-								<hr>
-									<div class="about-us-subtitle">Store Hours:</div>
-									Monday - Friday: 7:30am to 6:00 pm<br/>
-									Friday (trout season): 7:30am to 9:00pm<br/>
-									Saturday & Sunday: 7:30am to 6:00 pm
-								</div>
-							</div>
-						</div>
-						</div>
-					</div>
-				</div>
-		 </div>
-	</div>
+<div class="about-us-container">
+    <div class="sqs-layout">
+        <div class="row sqs-row normalize">
+            <div class="col sqs-col-14">
+                <div class="row row-footer">
+                    <div class="col sqs-col-6">
+                        <div class="sqs-block html-block sqs-block-html">
+                            <div class="sqs-block-content">
+                                <div class="footer-logos">
+                                    <img src="img/twitter2.png">
+                                    </img>
+                                    <img src="img/fblogo2.png">
+
+                                    </img>
+                                </div>
+                                <div class="about-us-title">PARADISE LODGE IS BOOKED EXCLUSIVELY<br/> THROUGH THE FLY SHOP&reg;</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col sqs-col-4">
+                        <div class="sqs-block html-block sqs-block-html">
+                            <div class="sqs-block-content">
+                                <hr style="margin-left: 0px !important"></hr>
+                                <div class="about-us-subtitle">The Fly Shop, Inc.</div>
+                                4140 Churn Creek Road<br/>
+                                Redding, CA 96002<br/>
+                                800-699-3474 www.theflyshop.com
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col sqs-col-4">
+                        <div class="sqs-block html-block sqs-block-html">
+                            <div class="sqs-block-content">
+                                <hr style="margin-left: 0px !important">
+                                <div class="about-us-subtitle">Store Hours:</div>
+                                Monday - Friday: 7:30am to 6:00 pm<br/>
+                                Friday (trout season): 7:30am to 9:00pm<br/>
+                                Saturday & Sunday: 7:30am to 6:00 pm
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
