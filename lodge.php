1532de558d78b4fc76c230281f2a8ac1e9d4d4e5
Alan Pomerantz <alan_pomerantz@hotmail.com>
2014-05-31 16:43:44 -0300

    Fixed fishing and lodging

--- lodge.php
+++ lodge.php
@@ -1,160 +1,160 @@
 <?php include_once("includes/head.php") ?>
 <div>
     <?php include_once("includes/header.php") ?>
-	<img style="width: 100% !important; "src="img/lodge/top-img.png"></img>
+    <img style="width: 100% !important; "src="img/lodge/top-img.png"></img>
 </div>
 <section id="separator-section">
-		<div class="sqs-layout">
-			  <div class="row sqs-row">
-				<div class="col sqs-col-14">
-					  <div class="row">
-									<div class="col sqs-col-14-margin">
-									<div class="sqs-block html-block sqs-block-html">
-										<div class="sqs-block-content">									
-											<hr></hr>
-											<span class="text-primary">
-											THE NEW LODGE BUILDING IS COMPLETELY REBUILT WITH ALL THE NEW FURNISHINGS. ALEX DETOMASO
-											IS VERY PROUD OF WHAT HE AND HIS CREW HAS ACCOMPLISHED IN BRINGING BACK PARADISE
-											LODGE BETTER THAN IT HAS EVER BEEN BEFORE</span>
-											<hr></hr>
-										</div>
-									</div>
-									</div>
-						</div>
-						<div class="row">
-									  <div class="col sqs-col-16 lodge-sect">
-										<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												  <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
-														<div class="sqs-block-content">
-														<h1>MAIN STRUCTURE</h1>
-														<h3><span style="font-size:9px"><b>GREAT VIEWS~ &nbsp; GREAT GATHERING SPACE</b></span></h3>
-														</div>
-													</div>
-												</div>
-												<div class="col sqs-col-8">
-													<div class="lodge-icons">
-													<img src="img/lodge/main-structure.png"></img>
-													</div>
-												</div>
-											</div>
-											<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-													<span>The main structure is a two-storey stucco building facing east and placed a short distance from the shore. One can observe tropical sunrises from its picture windows looking out across the sandy beach, past the reef and over the Caribbean Sea. Mornings don't start better than this. The main floor of the</span>
-													</div>
-												</div>
-											</div>
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-														<span>building is the great-room with dining area, bar and lounging area with comfortable couches and chairs. Behind a wall at the back of this room is the chef's domain, completely rebuilt with all new and modern culinary appliances and necessities</span>
-													</div>
-												</div>
-											</div>
-											</div>
-										</div>
-						</div>
-						<hr class="section-separator"></hr>
-						<div class="row">
-									  <div class="col sqs-col-16 lodge-sect">
-									  	<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												  <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
-														<div class="sqs-block-content">
-														<h1>GUEST ROOMS</h1>
-														<h3><span style="font-size:9px"><b>PRIVATE BATH~ &nbsp; SPACIOUS ACCOMMODATIONS</b></span></h3>
-														</div>
-													</div>
-												</div>
-												<div class="col sqs-col-8">
-													<div class="lodge-icons">
-													<img src="img/lodge/guest-rooms.png"></img>
-													</div>
-												</div>
-											</div>
-											<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-													<span>There are three separate guest rooms directly upstairs of the  great-room. Each room is furnished with double beds, air conditioned and has an  overhead fan for those nights when the breeze throught the windows from the ocean is just right without A/C. Every room has  a coffee maker for those early risers who need  that jolt</span>
-													</div>
-												</div>
-											</div>
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-														<span>of caffeine first thing in the AM and a refrigerator filled with drinks, water and snacks. Guest rooms are comfortable with plenty of room for storage and all have private bathrooms with  a shower, hot and cold water. Rooms are cleaned daily (there is no laundry service available)</span>
-													</div>
-												</div>
-											</div>
-											</div>
-										</div>
-						</div>
-						<hr class="section-separator"></hr>
-						<div class="row">
-									  <div class="col sqs-col-16 lodge-sect">
-									  	<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												  <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
-														<div class="sqs-block-content">
-														<h1>CASITA</h1>
-														<h3><span style="font-size:9px"><b>IDEAL FOR COUPLES~ &nbsp; PRIVATE AND SPACIOUS</b></span></h3>
-														</div>
-													</div>
-												</div>
-												<div class="col sqs-col-8">
-													<div class="lodge-icons">
-													<img src="img/lodge/guest-rooms.png"></img>
-													</div>
-												</div>
-											</div>
-											<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-													<span>There is detached cabana (called the "Casita") that is a little more private the the main rooms and is perfect  for couples. It is furnished similar to the <span>
-													</div>
-												</div>
-											</div>
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-														<span>other rooms. It is available to all guests on a first request basis.</span>
-													</div>
-												</div>
-											</div>
-											</div>
-										</div>
-						</div>
-						<hr class="section-separator"></hr>
-							<div class="row">
-									  <div class="col sqs-col-16 lodge-sect">
-											<div class="row sqs-row">
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-														<span>All the water is purified and bottled purified water is used for drinking  water. The electricity is supplied by generators and outlets are the same as in the  US. The generator operates through the night to ensure a comfortable sleeping envirioment  and only turned off for a brief period during the day while guests are fishing.</br> Paradise has an open bar and all drinks are on the  house. It is amply supplied with local Mexican beer, tequila, vodka, gin and rum for cocktails.</span>
-													</div>
-												</div>
-											</div>
-											<div class="col sqs-col-8">
-												<div class="sqs-block html-block sqs-block-html">
-													<div class="sqs-block-content">
-														<span> Deliciuos wines are served with dinner, also included in the  package price.</br> A computer is available for guest use to check/send emails, weather, etc. There is a phone at the lodge for customer use. There is a small fee for this service. </br> Pesos and US Dollars are gladly accepted methods of payment for gratuities and incidentals at the lodge. Credit cards are checks cannot be accepted.</span>
-													</div>
-												</div>
-											</div>
-											</div>
-										</div>
-						</div>
-					</div>
-				</div>
-		 </div>
-</section>
+    <div class="sqs-layout">
+        <div class="row sqs-row">
+            <div class="col sqs-col-14">
+                <div class="row">
+                    <div class="col sqs-col-14-margin normalize-sqs-col-14">
+                        <div class="sqs-block html-block sqs-block-html">
+                            <div class="sqs-block-content">									
+                                <hr></hr>
+                                <span class="text-primary">
+                                    THE NEW LODGE BUILDING IS COMPLETELY REBUILT WITH ALL THE NEW FURNISHINGS. ALEX DETOMASO
+                                    IS VERY PROUD OF WHAT HE AND HIS CREW HAS ACCOMPLISHED IN BRINGING BACK PARADISE
+                                    LODGE BETTER THAN IT HAS EVER BEEN BEFORE</span>
+                                <hr></hr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col sqs-col-16 lodge-sect">
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
+                                    <div class="sqs-block-content">
+                                        <h1>MAIN STRUCTURE</h1>
+                                        <h3><span style="font-size:9px"><b>GREAT VIEWS~ &nbsp; GREAT GATHERING SPACE</b></span></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col sqs-col-8">
+                                <div class="lodge-icons">
+                                    <img src="img/lodge/main-structure.png"></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html">
+                                    <div class="sqs-block-content">
+                                        <span>The main structure is a two-storey stucco building facing east and placed a short distance from the shore. One can observe tropical sunrises from its picture windows looking out across the sandy beach, past the reef and over the Caribbean Sea. Mornings don't start better than this. The main floor of the</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html">
+                                    <div class="sqs-block-content">
+                                        <span>building is the great-room with dining area, bar and lounging area with comfortable couches and chairs. Behind a wall at the back of this room is the chef's domain, completely rebuilt with all new and modern culinary appliances and necessities</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="section-separator"></hr>
+                <div class="row">
+                    <div class="col sqs-col-16 lodge-sect">
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
+                                    <div class="sqs-block-content">
+                                        <h1>GUEST ROOMS</h1>
+                                        <h3><span style="font-size:9px"><b>PRIVATE BATH~ &nbsp; SPACIOUS ACCOMMODATIONS</b></span></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col sqs-col-8">
+                                <div class="lodge-icons">
+                                    <img src="img/lodge/guest-rooms.png"></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html">
+                                    <div class="sqs-block-content">
+                                        <span>There are three separate guest rooms directly upstairs of the  great-room. Each room is furnished with double beds, air conditioned and has an  overhead fan for those nights when the breeze throught the windows from the ocean is just right without A/C. Every room has  a coffee maker for those early risers who need  that jolt</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html">
+                                    <div class="sqs-block-content">
+                                        <span>of caffeine first thing in the AM and a refrigerator filled with drinks, water and snacks. Guest rooms are comfortable with plenty of room for storage and all have private bathrooms with  a shower, hot and cold water. Rooms are cleaned daily (there is no laundry service available)</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="section-separator"></hr>
+                <div class="row">
+                    <div class="col sqs-col-16 lodge-sect">
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-5eee01554085f21573e2">
+                                    <div class="sqs-block-content">
+                                        <h1>CASITA</h1>
+                                        <h3><span style="font-size:9px"><b>IDEAL FOR COUPLES~ &nbsp; PRIVATE AND SPACIOUS</b></span></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col sqs-col-8">
+                                <div class="lodge-icons">
+                                    <img src="img/lodge/guest-rooms.png"></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row sqs-row">
+                            <div class="col sqs-col-8">
+                                <div class="sqs-block html-block sqs-block-html">
+                                    <div class="sqs-block-content">
+                                        <span>There is detached cabana (called the "Casita") that is a little more private the the main rooms and is perfect  for couples. It is furnished similar to the <span>
+                                                </div>
+                                                </div>
+                                                </div>
+                                                <div class="col sqs-col-8">
+                                                    <div class="sqs-block html-block sqs-block-html">
+                                                        <div class="sqs-block-content">
+                                                            <span>other rooms. It is available to all guests on a first request basis.</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                </div>
+                                                </div>
+                                                <hr class="section-separator"></hr>
+                                                <div class="row">
+                                                    <div class="col sqs-col-16 lodge-sect">
+                                                        <div class="row sqs-row">
+                                                            <div class="col sqs-col-8">
+                                                                <div class="sqs-block html-block sqs-block-html">
+                                                                    <div class="sqs-block-content">
+                                                                        <span>All the water is purified and bottled purified water is used for drinking  water. The electricity is supplied by generators and outlets are the same as in the  US. The generator operates through the night to ensure a comfortable sleeping envirioment  and only turned off for a brief period during the day while guests are fishing.</br> Paradise has an open bar and all drinks are on the  house. It is amply supplied with local Mexican beer, tequila, vodka, gin and rum for cocktails.</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col sqs-col-8">
+                                                                <div class="sqs-block html-block sqs-block-html">
+                                                                    <div class="sqs-block-content">
+                                                                        <span> Deliciuos wines are served with dinner, also included in the  package price.</br> A computer is available for guest use to check/send emails, weather, etc. There is a phone at the lodge for customer use. There is a small fee for this service. </br> Pesos and US Dollars are gladly accepted methods of payment for gratuities and incidentals at the lodge. Credit cards are checks cannot be accepted.</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                </div>
+                                                </div>
+                                                </section>
 
-<footer id="footer">
-<div class="back-to-top-link"><a href="#top"><span class="arrow"></span></a></div>
-<?php include_once("includes/footer-no-homepage.php") ?>
-</footer>
+                                                <footer id="footer">
+                                                    <div class="back-to-top-link"><a href="#top"><span class="arrow"></span></a></div>
+                                                            <?php include_once("includes/footer-no-homepage.php") ?>
+                                                </footer>
