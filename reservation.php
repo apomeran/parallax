0bcf87c5f692064b15ac4019e0f3e9d916ca35d0
unknown <alan_pomerantz@hotmail.com>
2014-06-04 09:44:53 -0300

    Added nights caption

--- reservation.php
+++ reservation.php
@@ -69,6 +69,9 @@
 
 
                                     <div class="sqs-block-content">
+                                        <div class="fixed-size-square">
+                                            <span>7 Nights/6 Days</span>
+                                        </div>
                                         <div class="inline-circles">
 
                                             <div class="circle">										
