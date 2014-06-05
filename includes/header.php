6e599a6d8e62fd9b9da4e5fdc31a6c6bc3451f79
apomeran <alan_pomerantz@hotmail.com>
2014-06-05 16:47:06 -0300

    Added Home Redirection on Logo

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 
 <div class="logo-title">
     <div class="logo2">
-        <img src="img/logo-paradise.png"></img>
+       <a href="./"> <img src="img/logo-paradise.png"></img> </a>
     </div>
     <div class="title-nav-wrapper">
         <div id="desktopNav">
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
  function scroll(element, parent) {
             $(parent).animate({scrollTop: $(parent).scrollTop() + $(element).offset().top - $(parent).offset().top}, {duration: 'slow', easing: 'swing'});
             $('html,body').animate({scrollTop: $(parent).offset().top}, {duration: 2000, easing: 'swing'});
-			}
+}
+
+function fadeIn(){
+
+}
