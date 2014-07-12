00bd4a993ad1c0f2e15b15a32303b307826f70e1
Alan Pomerantz <alan_pomerantz@hotmail.com>
2014-07-12 14:48:53 -0300

    Finished Reports Page

---
+++ reports.php
@@ -0,0 +1,312 @@
+<?php include_once("includes/head.php") ?>
+<div>
+	<div id="top-page" style="height: 288px; background-color: #B38845; width: 100% !important;">
+		<?php include_once("includes/header.php") ?>
+				<img class="image-center report-header" src="img/report/report.png"></img>
+	</div>
+
+	<div class="month-container">
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				SEPTEMBER
+			</div>
+			<div class="month-sub-caption">
+				2014
+			</div>
+			<div class="month-options">
+				<a href="#">FIRST WEEK</a>
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				<a href="#">FOURTH WEEK</a>
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				OCTOBER
+			</div>
+			<div class="month-sub-caption">
+				2014
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				NOVEMBER
+			</div>
+			<div class="month-sub-caption">
+				2014
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				FEBRUARY
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				APRIL
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				MAY
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				JUNE
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				JULY
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>	
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				AUGUST
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>	
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				SEPTEMBER
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>	
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				OCTOBER
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>	
+		<div class="monthly-bucket">
+			<div class="month-header">
+				<img src="img/report/icon.png"></img>
+			</div>
+			<div class="month-caption">
+				NOVEMBER
+			</div>
+			<div class="month-sub-caption">
+				2015
+			</div>
+			<div class="month-options">
+				FIRST WEEK
+			</div>
+			<div class="month-options">
+				SECOND WEEK
+			</div>
+			<div class="month-options">
+				THIRD WEEK
+			</div>
+			<div class="month-options">
+				FOURTH WEEK
+			</div>
+			<hr class="separator-month">
+		</div>	
+		
+		<div style="width:100%; height:160px;">
+		</div>
+		
+	</div>	
+</div>
+<footer id="footer">
+    <div class="back-to-top-link" onClick="scroll('#footer', '#top-page')">
+
+        <a href="#top">
+            <span class="arrow">
+            </span>
+        </a>
+    </div>
+    <?php include_once("includes/footer-no-homepage.php") ?>
+</footer>
