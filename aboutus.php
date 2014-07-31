64f919edd53723e70faaafa15f0882a00fc8aa02
apomeran <alan_pomerantz@hotmail.com>
2014-07-31 17:59:59 -0300

    Finished About Us

--- aboutus.php
+++ aboutus.php
@@ -4,7 +4,7 @@
     <img style="width: 100% !important; "src="img/about-us/top-img.png"></img>
 </div>
 <section id="separator-section">
-    
+
     <div class="sqs-layout sqs-grid-12 columns-12">
         <div class="row sqs-row">
             <div class="col sqs-col-12 span-12">
@@ -45,29 +45,25 @@
     </div> 
     <section id="separator-section">
 
-        
+
         <div class="sqs-layout sqs-grid-12 columns-12">
             <div class="row sqs-row">
                 <div class="col sqs-col-12 span-12">
                     <div class="sqs-block html-block sqs-block-html">
-                        <div class="sqs-block-content playfair">
-                            <div class="title-white-bar">WELCOME TO PARADISE</div>
-                            <h3><span style="font-size:12px"><b>GREAT TO SEE YOU</b></span></h3>
-                        </div>
+
                     </div>
                     <div class="row sqs-row">
                         <div class="col sqs-col-6 span-6">
                             <div class="sqs-block html-block sqs-block-html">
                                 <div class="sqs-block-content">
-                                    <span>Paradise Lodge is aplty named. What was at first hailed as the lodge most onsistently productive for tarpon and snook includes outstanding bonefishing in the most scenic and remote flats of the Mexican Yucatan. Add to this excellent possibilities of</span>
+                                    <span>The new, completely rebuilt Paradise Lodge has re-opened its doors after its complete destruction by Hurricane Dean in August of 2007. It offers wilderness saltwater fishing from complete comfort in luxury accommodations on the Caribbean Coast near the border with Belize. It looks out over the sea and its part of the second longest barrier reef in the world. It is small catering to only 6 anglers at one time, with 4 rooms to provide space for non anglers as well as anglers. The whole idea behind the rebuild was to make individual attention the first priority of the lodge and staff. The meals are served taking advantage of the chef's expertise in preparing Mexican and international cuisine. All of the drinks are on the house and the bar is always open. It is located where it is on purpose. It was originally built by Dial Duncan about 20 years ago and he chose its foundation to be laid </span>
                                 </div>
                             </div>
                         </div>
                         <div class="col sqs-col-6 span-6">
                             <div class="sqs-block html-block sqs-block-html">
                                 <div class="sqs-block-content">
-                                    <span>hooking up with the most elusive of flats fish; the permit. Paradise Lodge is one of The Fly Shop's signature destinations. This is because it epitomizes everything we want to see in a lodge in this area. It wants for nothing</span>
-                                    <p><span href="#" style="font-size:small"><b>Keep Reading ...</b></span></p>
+                                    <span>smack dab in the middle of the many tarpon and snook lakes just inland from the sea and connected to the ocean by cenotes (pronounced say- no'-tase) and the two bays, Chetumal (to the south of the lodge) and Espiritu Santo (to the north). He and subsequent owners, along with lodge guides have been exploring those waters ever since. It is probably the best destination in the Yucatan to seek the hard won goal of a Yucatan Slam. This is landing a bonefish, tarpon, permit and snook in one day. Easy to say but extremely difficult to achieve. If you add the ocean waters in the front of the lodge (accessible on calm days) to the two bays and the nearly dozen lakes, there are 4 distinctly different waters to fish in which to fish 4 distinctly different fish. In addition to the fishes mentioned above, there are jack crevalle, barracuda, snapper and others to talk into eating your fly offering.</span>
                                 </div>
                             </div>
                         </div>
@@ -86,76 +82,76 @@
         </span>
     </div> 
     <section id="separator-section">
-      
+
         <div class="sqs-layout">
             <div class="row sqs-row">
                 <div class="col sqs-col-12 span-12">
                     <div class="sqs-block html-block sqs-block-html">
-                        <div class="sqs-block-content playfair">
-                            <div class="title-white-bar">WELCOME TO PARADISE</div>
-                            <h3><span style="font-size:12px"><b>GREAT TO SEE YOU</b></span></h3>
-                        </div>
                     </div>
                     <div class="row sqs-row">
                         <div class="col sqs-col-6 span-6">
                             <div class="sqs-block html-block sqs-block-html">
                                 <div class="sqs-block-content">
-                                    <span>Paradise Lodge is aplty named. What was at first hailed as the lodge most onsistently productive for tarpon and snook includes outstanding bonefishing in the most scenic and remote flats of the Mexican Yucatan. Add to this excellent possibilities of</span>
+                                    <span>The guides are all experienced and were with the lodge before the hurricane. They know the waters well and know how to position a fly fisher to get the best shots at whatever fish he/she has targeted. The boats are all in tip-top condition and are adapted to the local water and fishing conditions. The vehicles are all well maintained, air conditioned and provide the most comfortable ride possible to fishing each day.smack dab in the middle of the many tarpon and snook lakes just inland from the sea and connected to the ocean by cenotes (pronounced say- no'-tase) and the two bays, Chetumal (to the south of the lodge) and Espiritu Santo (to the north). He and subsequent owners, along with lodge </span>
                                 </div>
                             </div>
                         </div>
                         <div class="col sqs-col-6 span-6">
                             <div class="sqs-block html-block sqs-block-html">
                                 <div class="sqs-block-content">
-                                    <span>hooking up with the most elusive of flats fish; the permit. Paradise Lodge is one of The Fly Shop's signature destinations. This is because it epitomizes everything we want to see in a lodge in this area. It wants for nothing</span>
-                                    <p><span href="#" style="font-size:small"><b>Keep Reading ...</b></span></p>
+                                    <span>guides have been exploring those waters ever since. It is probably the best destination in the Yucatan to seek the hard won goal of a Yucatan Slam. This is landing a bonefish, tarpon, permit and snook in one day. Easy to say but extremely difficult to achieve. If you add the ocean waters in the front of the lodge (accessible on calm days) to the two bays and the nearly dozen lakes, there are 4 distinctly different waters to fish in which to fish 4 distinctly different fish. In addition to the fishes mentioned above, there are jack crevalle, barracuda, snapper and others to talk into eating your fly offering.</span>
                                 </div>
                             </div>
                         </div>
-						<div class="sqs-block html-block sqs-block-html">
-                        <div class="sqs-block-content playfair">
-                            <div class="title-white-bar">GUIDE STAFF</div>
-                            <h3><span style="font-size:12px"><b></b></span></h3>
-                        </div>
-                    </div>
+
+
                     </div>
                 </div>
 
             </div>
         </div>
+                    <div class="title-white-bar" style="margin-left: 10%;margin-bottom: 4%;">GUIDE STAFF</div>
 
     </section>
 
 </div>
 <link rel="stylesheet" type="text/css" href="css/style_common.css" />
 <link rel="stylesheet" type="text/css" href="css/style3.css" />
-<br><br>
+
 <div class="circles-container">
-   <div style="float:center">
-		<div class="view view-third">
-			 <img class="the-pic" src="img/about-us/guide-1.png" />
-			  <div class="mask">
-				  <h2>ALEJANDRO</h2>
-			  </div>
-		</div>
-		<div class="view view-third">
-			 <img class="the-pic" src="img/about-us/guide-2.png" />
-			  <div class="mask">
-				  <h2>FERNANDO</h2>
-			  </div>
-		</div><div class="view view-third">
-			 <img class="the-pic" src="img/about-us/guide-3.png" />
-			  <div class="mask">
-				  <h2>LUIS</h2>
-			  </div>
-		</div>
-		<div class="view view-third">
-			 <img class="the-pic" src="img/about-us/guide-4.png" />
-			  <div class="mask">
-				  <h2>BARTOLO</h2>
-			  </div>
-		</div>
-	</div>	
+    <div style="float:center">
+        <div class="view view-third caption-circles">
+            <img class="the-pic" src="img/about-us/guide-1.png" />
+			<div>
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			ALEJANDRO
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			</div>
+        </div>
+        <div class="view view-third caption-circles">
+            <img class="the-pic" src="img/about-us/guide-2.png" />
+		<div>
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			FERNANDO
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			</div>
+        </div><div class="view view-third caption-circles">
+            <img class="the-pic" src="img/about-us/guide-3.png" />
+			<div>
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			LUIS
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			</div>
+        </div>
+        <div class="view view-third caption-circles">
+            <img class="the-pic" src="img/about-us/guide-4.png" />
+			<div>
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			BARTOLO
+			<hr class="my-hr" style="margin-left: auto;margin-right: auto;">
+			</div>
+        </div>
+    </div>	
 </div>	
 
 
